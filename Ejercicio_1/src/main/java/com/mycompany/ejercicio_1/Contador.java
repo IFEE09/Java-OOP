fec3eb381610b6eba970becd84bc3a1079883d8c
IFEE09 <ivanfaridespadas@gmail.com>
2022-09-01 18:45:15 -0500

    agregando validación de cuenta negativa

--- Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Contador.java
+++ Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Contador.java
@@ -1,17 +1,37 @@
 
 package com.mycompany.ejercicio_1;
+
+import java.util.Scanner;
+
 public class Contador {
    
     private float cuenta = 0;
     
     //Constructor con parametros
     public Contador(float cuenta) {
-        boolean esNegativo = false;
-        if (cuenta < 0) {
-            int opcion;
-            esNegativo = true;
-        }
         
+        if (cuenta < 0) {
+            System.out.println("La cuenta está siendo creada en negativos, es correcto?");
+            System.out.println("1. Si");
+            System.out.println("2. No");
+            System.out.print("Digite una opción: ");
+            Scanner opcion_Digitada = new Scanner(System.in);
+            int opcion = opcion_Digitada.nextInt();
+            switch (opcion) {
+                case 1:
+                    this.cuenta = cuenta;
+                    System.out.println("La cuenta ha sido creada con éxito con " + this.cuenta + " de saldo. Gracias");
+                    break;
+                case 2:
+                    System.out.println("Error, la cuenta no ha sido creada. Gracias.");
+                    break;
+                default:
+                    System.out.println("Opción invalida.");
+                    throw new AssertionError();
+            }
+        } else {
+            this.cuenta = cuenta;
+        }
     }
     
     //Constructor copia
--- Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Ejercicio_1.java
+++ Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Ejercicio_1.java
@@ -6,12 +6,14 @@ public class Ejercicio_1 {
         
         Contador farid = new Contador(1000);
         Contador ruth = new Contador(1000);
+        Contador negativo = new Contador(-200);
         
         farid.decrementarCuenta();
         ruth.aumentarCuenta();
         
         System.out.println("La cuenta actual de Farid es " + farid.getCuenta());
         System.out.println("La cuenta actual de Ruth es " + ruth.getCuenta());
+        System.out.println("La cuenta actual de Negativo es " + negativo.getCuenta());
 
     }
 }
