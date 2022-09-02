1063b17b18b275c026db9c06b7e3431e8859ed11
IFEE09 <ivanfaridespadas@gmail.com>
2022-09-01 20:46:35 -0500

    terminando el constructor copia

--- Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Ejercicio_1.java
+++ Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Ejercicio_1.java
@@ -7,13 +7,17 @@ public class Ejercicio_1 {
         Contador farid = new Contador(1000);
         Contador ruth = new Contador(1000);
         Contador negativo = new Contador(-200);
+        Contador copia = new Contador(ruth);
         
         farid.decrementarCuenta();
         ruth.aumentarCuenta();
+        copia.decrementarCuenta();
         
         System.out.println("La cuenta actual de Farid es " + farid.getCuenta());
         System.out.println("La cuenta actual de Ruth es " + ruth.getCuenta());
         System.out.println("La cuenta actual de Negativo es " + negativo.getCuenta());
+        System.out.println("La cuenta actual de copia es " + copia.getCuenta());
+
 
     }
 }
